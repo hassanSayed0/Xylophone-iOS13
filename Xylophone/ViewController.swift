674c6718392f398e751ed792bf748634a06eea25
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2023-01-31 13:28:38 +0200

    task
    done

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound( nameWave: sender.currentTitle)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.2) {
+            sender.alpha = 1.0
+        }
+    }
     
-
+    func playSound( nameWave: String?) {
+        let url = Bundle.main.url(forResource: nameWave, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
